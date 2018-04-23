77bc96bfdf890a5b8b9e2a9ca893fb3ca6aea73e
paas <paas@cse-codegen-backend-2377316216-nb2fk>
2018-04-23 02:06:47 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/tank042301/Tank042301Application.java
@@ -0,0 +1,11 @@
+package com.service.tank042301;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class Tank042301Application {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
+        BeanUtils.init();
+    }
+}
