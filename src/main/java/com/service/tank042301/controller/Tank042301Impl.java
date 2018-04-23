0459ee56b19f46d3b196e734601993daba350665
paas <paas@cse-codegen-backend-2377316216-nb2fk>
2018-04-23 02:44:41 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/tank042301/controller/Tank042301Impl.java
@@ -0,0 +1,28 @@
+package com.service.tank042301.controller;
+
+
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CseSpringDemoCodegen", date = "2018-04-23T02:44:41.082Z")
+
+@RestSchema(schemaId = "tank042301")
+@RequestMapping(path = "/tank042301", produces = MediaType.APPLICATION_JSON)
+public class Tank042301Impl {
+
+    @Autowired
+    private Tank042301Delegate userTank042301Delegate;
+
+
+    @RequestMapping(value = "/helloworld",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String helloworld( @RequestParam(value = "name", required = true) String name){
+
+        return userTank042301Delegate.helloworld(name);
+    }
+
+}
